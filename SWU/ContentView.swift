38b63bbfa3dd0d86f21c7bc87fd5cbbefce30076
Scott Ha <scott@noiz.gg>
2024-02-29 14:55:24 -0800

    Initial Commit

---
+++ SWU/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SWU
+//
+//  Created by Scott Ha on 2/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SWU/SWUApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWUApp.swift
+//  SWU
+//
+//  Created by Scott Ha on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SWUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
